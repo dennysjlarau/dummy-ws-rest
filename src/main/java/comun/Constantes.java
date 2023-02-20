a45ec2db6f4245fdc8400d078e221e695a115fe2
dennysjlarau <dennysjlarau@gmail.com>
2023-02-20 13:31:22 -0500

    mejora servicios y se agregan capacidade de respuesta y cabeceras

---
+++ src/main/java/comun/Constantes.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package comun;
+
+/**
+ * @author denny
+ *
+ */
+public final class Constantes {
+
+	public final static String PATH_ARCHIVO = "c:/var/dummy/";
+	public final static String RESPUESTA_CUERPO = "body";
+	public final static String RESPUESTA_CABECERA = "headers";
+}
